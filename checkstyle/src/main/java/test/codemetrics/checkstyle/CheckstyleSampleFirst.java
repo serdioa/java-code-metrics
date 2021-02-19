7672f4564bf9b55b45d2b07aa58415997fc5f049
serdioa <serdioa@t-online.de>
2021-02-19 13:26:02 +0100

    Added configuration and samples for suppressing Checkstyle warnings

--- checkstyle/src/main/java/test/codemetrics/checkstyle/CheckstyleSampleFirst.java
+++ checkstyle/src/main/java/test/codemetrics/checkstyle/CheckstyleSampleFirst.java
@@ -6,12 +6,61 @@ package test.codemetrics.checkstyle;
  */
 public class CheckstyleSampleFirst {
 
+    /**
+     * A method with a valid name which will not trigger a Checkstyle warning.
+     *
+     * @return a dummy value.
+     */
     public int validMethodName() {
         return 0;
     }
 
 
+    /**
+     * A method with a name which violates Checkstyle rules and will not trigger a Checkstyle warning.
+     *
+     * @return a dummy value.
+     */
     public int INVALID_METHOD_NAME() {
         return 0;
     }
+
+
+    /**
+     * Another method with a name which violates Checkstyle rules, but the Checkstyle warning is suppressed with an
+     * annotation:
+     * <pre>{@code
+     * @SuppressWarnings("checkstyle:methodname")
+     * public int INVALID_METHOD_NAME_SUPPRESSED_WITH_ANNOTATION() {
+     *     return 0;
+     * }
+     * }
+     * </pre>
+     *
+     * @return a dummy value.
+     */
+    @SuppressWarnings("checkstyle:methodname")
+    public int INVALID_METHOD_NAME_SUPPRESSED_WITH_ANNOTATION() {
+        return 0;
+    }
+
+
+    /**
+     * And yet another method with a name which violates Checkstyle rules, but the Checkstyle warning is suppressed with
+     * a comment:
+     * <pre>
+     * // CHECKSTYLE.OFF: MethodName
+     * public int INVALID_METHOD_NAME_SUPPRESSED_WITH_COMMENT() {
+     *     return 0;
+     * }
+     * // CHECKSTYLE.ON: MethodName
+     * </pre>
+     *
+     * @return a dummy value.
+     */
+    // CHECKSTYLE.OFF: MethodName
+    public int INVALID_METHOD_NAME_SUPPRESSED_WITH_COMMENT() {
+        return 0;
+    }
+    // CHECKSTYLE.ON: MethodName
 }
