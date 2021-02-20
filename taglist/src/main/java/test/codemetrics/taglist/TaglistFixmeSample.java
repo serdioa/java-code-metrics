a2515110aea512ef367cfd8486aa50459929f62f
serdioa <serdioa@t-online.de>
2021-02-20 12:56:11 +0100

    Added Taglist configuration

---
+++ taglist/src/main/java/test/codemetrics/taglist/TaglistFixmeSample.java
@@ -0,0 +1,13 @@
+package test.codemetrics.taglist;
+
+
+/**
+ * Sample class for testing Taglist with a "fixme" comment.
+ */
+public class TaglistFixmeSample {
+
+    public int zero() {
+        // FIXME: return 0, as the method name promises.
+        return 1;
+    }
+}
---
+++ taglist/src/main/java/test/codemetrics/taglist/TaglistTodoSample.java
@@ -0,0 +1,13 @@
+package test.codemetrics.taglist;
+
+
+/**
+ * Sample class for testing Taglist with a "todo" comment.
+ */
+public class TaglistTodoSample {
+
+    public int zero() {
+        // TODO: implement
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
