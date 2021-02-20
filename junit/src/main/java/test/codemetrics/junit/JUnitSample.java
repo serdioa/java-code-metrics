e0ad08d31bee297488e19f6cd532f4316524236a
serdioa <serdioa@t-online.de>
2021-02-20 13:22:59 +0100

    Added Surefire configuration

---
+++ junit/src/main/java/test/codemetrics/junit/JUnitSample.java
@@ -0,0 +1,26 @@
+package test.codemetrics.junit;
+
+
+/**
+ * Sample class for testing JUnit.
+ */
+public class JUnitSample {
+
+    public int add(int i, int j) {
+        return i + j;
+    }
+
+
+    public int subtract(int i, int j) {
+        return i - j;
+    }
+
+
+    public int branch(int i, int j) {
+        if (i % 2 == 0) {
+            return this.add(i, j);
+        } else {
+            return this.subtract(i, j);
+        }
+    }
+}
---
+++ junit/src/test/java/test/codemetrics/junit/JUnitSampleTest.java
@@ -0,0 +1,34 @@
+package test.codemetrics.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+
+
+/**
+ * Sample class for testing JUnit.
+ */
+public class JUnitSampleTest {
+
+    private JUnitSample sample;
+
+
+    @Before
+    public void setUp() {
+        this.sample = new JUnitSample();
+    }
+
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, this.sample.add(2, 3));
+    }
+
+
+    @Test
+    public void testBranch() {
+        assertEquals(5, this.sample.add(2, 3));
+    }
+}
